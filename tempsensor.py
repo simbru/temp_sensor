1b258c032a20417115a2e832a5ab41ef0f0d80d5
Simen Bruøygard <simen.bruoy@gmail.com>
2024-07-10 14:12:46 +0100

    Convenience, improvements, bits n bops

--- tempsensor.py
+++ tempsensor.py
@@ -2,7 +2,16 @@ import adafruit_dht
 from board import D4 as pin
 import time 
 import datetime
+import configparser
+import pathlib
+import os
 
+# Read config
+config_loc = pathlib.Path(r"/home/weatherstation/.config.ini")
+config = configparser.ConfigParser()
+config.read(config_loc)
+
+# Sensor stuff
 sensor = adafruit_dht.DHT22
 dht_device = adafruit_dht.DHT22(pin)
 
@@ -15,6 +24,18 @@ def log():
         humidity = ""
     return temperature, humidity
 
+def test_config_access(config, diagnostic_print = True):
+    if diagnostic_print is True:
+        print("Running from:", os.getcwd())
+        print("File at:", config_loc)
+        print("File exists:", config_loc.exists())
+        print("Default:", dict(config["DEFAULT"]))
+        print("Other sections:", config.sections())
+    return 0
+
+def test_csv_access():
+    return 0
+
 def start_log(running = True, to_csv = True, _print = True):
     while running == True:
         temp, hum = log()
@@ -23,4 +44,5 @@ def start_log(running = True, to_csv = True, _print = True):
             print(temp, hum)
 
 if __name__ == "__main__":
-    start_log()
+    #start_log()
+    print(test_config_access(config))
---
+++ test_me.py
@@ -0,0 +1,15 @@
+
+
+# unit testing with pytest
+
+def try_all_imports():
+    import adafruit_dht
+    from board import D4 as pin
+    import time 
+    import datetime
+    import configparser
+    import pathlib
+    import os
+
+if __name__ == "__main__":
+    try_all_imports
