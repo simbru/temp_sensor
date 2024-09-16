d451fe60926aaa4fd6538e0af4008e8abe5f2042
Simen Bruøygard <simen.bruoy@gmail.com>
2024-09-16 15:09:06 +0100

    Working shiny express implementation

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,22 +2,20 @@ import pandas as pd
 import os
 import csv
 import pathlib
-from shiny import App, Inputs, Outputs, Session, reactive, ui, render
-from shinywidgets import output_widget, render_plotly
-import plotly.express as px
-import plotly.graph_objects as go
 from shiny import reactive
 import subprocess
-
-
+from shiny.express import input, ui, render
+from shinywidgets import render_bokeh
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+import time
 # Local imports
 import io_funcs
 import tempsensor
 import datetime
 
-# SHINYDIR = pathlib.Path(os.getcwd())
-# TEMPSENSDIR = SHINYDIR.parent
-# # os.chdir(TEMPSENSDIR)
+_ = subprocess.Popen(["python", "tempsensor.py"]) #assign to _ to mitigate Shiny express type error 
+time.sleep(2) # give time for tempsensor to spin up
 
 CONFIG = io_funcs.fetch_config()
 print(dict(CONFIG["DEFAULT"]))
@@ -25,59 +23,32 @@ SAVEPATH = pathlib.Path(CONFIG["DEFAULT"]["outputfile"])
 print("Save path:", SAVEPATH)
 print("Exists:", SAVEPATH.exists())
 
-#
-def plot_temp_timeseries(df, window_size=10, leading_s=20, update_view=True):
-    # Ensure 'time' is in datetime format
-    df['time'] = pd.to_datetime(df['time'])
+with ui.card():
+    @reactive.file_reader(SAVEPATH, interval_secs=2)
+    def fetch_data():
+        # Force pandas to re-read the file without caching
+        df = pd.read_csv(SAVEPATH, engine='python')
+        return df
 
-    fig = px.line(
-        df,
-        x="time",
-        y="temperature",
-    ).update_layout(
-        yaxis_title="Temperature",
-        xaxis_title="Date time",
-    )
-    fig.update_yaxes(rangemode="tozero")
-    if update_view:
-        #Get the max time and set that as the focus point of scrolling
-        max_time = df['time'].max() + pd.to_timedelta(leading_s, unit='s')
-        min_time = max_time - pd.Timedelta(minutes=window_size)
-        fig.update_xaxes(range=[min_time, max_time])
-    return fig
+    p = figure(title = "Test", x_axis_label = "Time", y_axis_label = "Temperature",
+            x_axis_type = "datetime",)
+    df = pd.read_csv(SAVEPATH, engine='python') # first read the file once to get start state
+    df["time"] = pd.to_datetime(df["time"])
+    source = ColumnDataSource(data=df)
+    # temperature, time = df["temperature"], df["time"]
+    plot_line = p.line(x = "time", y = "temperature", source = source, color = "red")
 
-def plot_hume_timeseries(df, window_size=10, leading_s=20, 
-        yspan_percent = .10, ymax_recency_h = 6, update_view=True):
-    fig = px.line(
-        df,
-        x="time",
-        y="humidity",
-    ).update_layout(
-        yaxis_title="Humiditiy",
-        xaxis_title="Date time",
-    )
-    # max_hume_recent = df['humidity'].max()
-    # # Calculate the y-axis range
-    # fig.update_yaxes(range = )
-    # Calculate the x-axis range
-    if update_view is True:
-        #Get the max time and set that as the focus point of scrolling
-        max_time = df['time'].max() + pd.to_timedelta(leading_s, unit='s')
-        min_time = max_time - pd.Timedelta(minutes=window_size)
-        fig.update_xaxes(range=[min_time, max_time])
-    return fig
+    @render_bokeh
+    def init_plot():
+        return p
 
-def server(input: Inputs, output: Outputs, session: Session):
-
-    auto_scroll = reactive.Value(True) 
-
-    @render_plotly
-    def init_temp_plot():
-        return go.FigureWidget()
-
-    @render_plotly
-    def init_hum_plot():
-        return go.FigureWidget()
+    @render_bokeh
+    @reactive.file_reader(SAVEPATH, interval_secs=2)
+    def update_plot():
+        df = pd.read_csv(SAVEPATH, engine='python')
+        temperature, time = df["temperature"], df["time"]
+        df["time"] = pd.to_datetime(df["time"])
+        source.data = df
 
     @render.text
     @reactive.file_reader(SAVEPATH)
@@ -89,61 +60,3 @@ def server(input: Inputs, output: Outputs, session: Session):
             parsed_date = datetime.datetime.strptime(curr_string, '%Y-%m-%d %H:%M:%S.%f')
             formatted_date = parsed_date.strftime('%B %d, %Y at %I:%M:%S %p')
             return f"Time: {formatted_date}"
-
-    @reactive.file_reader(SAVEPATH, interval_secs=2)
-    def fetch_data():
-        # Force pandas to re-read the file without caching
-        df = pd.read_csv(SAVEPATH, engine='python')
-        return df
-        
-    @reactive.effect
-    def update_temp_plot():
-        df = fetch_data()
-        #Generate figure
-        fig = plot_temp_timeseries(df, update_view=auto_scroll.get())
-        if auto_scroll.get():
-            with init_temp_plot.widget.batch_animate():
-                #Update the plot layout and its data
-                init_temp_plot.widget.update(layout=fig.layout, data=fig.data)
-        else:
-            init_temp_plot.widget.update(data=fig.data)
-    
-    @reactive.effect
-    def update_hum_plot():
-        df = fetch_data()
-        #Generate figure
-        fig = plot_hume_timeseries(df, update_view=auto_scroll.get())
-        if auto_scroll.get():
-            with init_hum_plot.widget.batch_animate():
-                #Update the plot layout and its data
-                init_hum_plot.widget.update(layout=fig.layout, data=fig.data)
-        else:
-            init_hum_plot.widget.update(data=fig.data)
-
-    @reactive.Effect
-    @reactive.event(input.pause_scroll)
-    def pause_scroll():
-        auto_scroll.set(False)  # Stop auto-scrolling when the button is pressed
-
-    @reactive.Effect
-    @reactive.event(input.resume_scroll)
-    def resume_scroll():
-        auto_scroll.set(True)  # Resume auto-scrolling when the button is pressed
-
-    
-app_ui = ui.page_fluid(
-    ui.card(
-            ui.card_header(f"Temperature over time ({SAVEPATH})"),
-            output_widget("init_temp_plot")
-            ),
-    ui.card(
-        ui.card_header(f"Humiditiy over time ({SAVEPATH})"),
-        output_widget("init_hum_plot")
-        ), 
-    ui.output_text("time", inline = True),
-    ui.input_action_button("pause_scroll", "Pause Auto-Scroll"),
-    ui.input_action_button("resume_scroll", "Resume Auto-Scroll")
-)
-
-subprocess.Popen(["python", "tempsensor.py"], )
-app = App(app_ui, server)
